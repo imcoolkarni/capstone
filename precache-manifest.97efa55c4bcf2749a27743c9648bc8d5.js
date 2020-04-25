0b5af35f5839a384392398303fa85f6a48e776fa
abc <abc@example.com>
2020-04-25 14:56:52 +0530

    Updates

---
+++ precache-manifest.97efa55c4bcf2749a27743c9648bc8d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f86db7bb7d9565aeb068c890c882dff",
+    "url": "/Dummy/index.html"
+  },
+  {
+    "revision": "d8b896b5a0a425c72b18",
+    "url": "/Dummy/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f33e73fa53177011c7bb",
+    "url": "/Dummy/static/css/main.94110e85.chunk.css"
+  },
+  {
+    "revision": "d8b896b5a0a425c72b18",
+    "url": "/Dummy/static/js/2.7b0b8a90.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Dummy/static/js/2.7b0b8a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33e73fa53177011c7bb",
+    "url": "/Dummy/static/js/main.8768b329.chunk.js"
+  },
+  {
+    "revision": "8707ae1b08dd558607b9",
+    "url": "/Dummy/static/js/runtime-main.8028f915.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Dummy/static/media/logo.5d5d9eef.svg"
+  }
+]);
